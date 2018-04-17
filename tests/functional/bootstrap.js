361613fded6d30297e636670c9a3746f47851501
Roderick Hsiao <roderick.hsiao@gotinder.com>
2018-04-17 17:03:45 +0800

    Include async

--- tests/functional/bootstrap.js
+++ tests/functional/bootstrap.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { expect } from 'chai';
+import async from 'async';
 
 import leIE8 from '../../src/lib/leIE8';
 
@@ -10,6 +11,7 @@ import SubscribeDemo from './subscribe-functional';
 
 window.expect = expect;
 window.leIE8 = leIE8;
+window.async = async;
 
 const container = document.getElementById('container');
 ReactDOM.render(React.createElement(SubscribeDemo, null), container);
