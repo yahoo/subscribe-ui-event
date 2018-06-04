d0c6d3be4c7c11cc8bc46675456ae8cb0e1aacba
Roderick Hsiao <roderick.hsiao@gotinder.com>
2018-06-04 15:49:55 -0700

    Transpile es6 features to es5

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,20 +115,21 @@ module.exports = function (grunt) {
             [
               'env',
               {
-                targets: {
-                  browsers: [
-                    'last 2 Chrome versions',
-                    'not Chrome < 60',
-                    'last 2 Safari versions',
-                    'not Safari < 10.1',
-                    'last 2 iOS versions',
-                    'not iOS < 10.3',
-                    'last 2 Firefox versions',
-                    'not Firefox < 54',
-                    'last 2 Edge versions',
-                    'not Edge < 15'
-                  ]
-                },
+                // Enable once most of the browser support es module
+                // targets: {
+                //   browsers: [
+                //     'last 2 Chrome versions',
+                //     'not Chrome < 60',
+                //     'last 2 Safari versions',
+                //     'not Safari < 10.1',
+                //     'last 2 iOS versions',
+                //     'not iOS < 10.3',
+                //     'last 2 Firefox versions',
+                //     'not Firefox < 54',
+                //     'last 2 Edge versions',
+                //     'not Edge < 15'
+                //   ]
+                // },
                 useBuiltIns: 'entry',
                 modules: false
               }
